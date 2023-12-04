831c5af46616706dfafda65ee11aa84e765b338b
Reinhaa5 <94755067+Reinhaa5@users.noreply.github.com>
2023-12-04 14:14:49 -0500

    Added interaction to button with JS

--- src/scripts/quizCalc.js
+++ src/scripts/quizCalc.js
@@ -1,20 +1,50 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const yesButton = document.getElementById('gameInteractYes')
-	const noButton = document.getElementById('gameInteractNo')
-	let selectedButton = null // Initially, no button is selected
+document.addEventListener('DOMContentLoaded', function() {
+  const yesButtons = document.querySelectorAll('.yesButton');
+  const noButtons = document.querySelectorAll('.noButton');
+  const buttonWithImageContainers = document.querySelectorAll('.buttonWithImage');
+  const noneOfTheFeaturesButton = document.getElementById('gameStoreNone').closest('.buttonWithImage');
+  let selectedOptions = []; // Initially, no buttons are selected
 
-	yesButton.addEventListener('click', function () {
-		selectedButton = 'True'
-		yesButton.classList.add('active')
-		noButton.classList.remove('active')
-	})
+  yesButtons.forEach((yesButton, index) => {
+    yesButton.addEventListener('click', function() {
+      selectedOptions[index] = 'True';
+      toggleActiveState(index, yesButtons, noButtons);
+    });
+  });
 
-	noButton.addEventListener('click', function () {
-		selectedButton = 'False'
-		noButton.classList.add('active')
-		yesButton.classList.remove('active')
-	})
+  noButtons.forEach((noButton, index) => {
+    noButton.addEventListener('click', function() {
+      selectedOptions[index] = 'False';
+      toggleActiveState(index, noButtons, yesButtons);
+    });
+  });
 
-	// You can add an event listener to process the selected option
-	// For example, you can use the 'selectedButton' variable in a function to perform an action based on the selected value.
-})
+  buttonWithImageContainers.forEach(container => {
+    container.addEventListener('click', function() {
+      const selectedValue = container.querySelector('input[type="button"]').value;
+	  console.log(container);
+	  console.log(selectedValue);
+
+	  if (selectedValue !== 'None of the features') {
+        noneOfTheFeaturesButton.classList.remove('active');
+      }
+
+      if (selectedValue === 'None of the features.') {
+        buttonWithImageContainers.forEach(otherContainer => {
+          if (otherContainer !== container) {
+            otherContainer.classList.remove('active');
+          } 
+        });
+      }
+	  container.classList.add('active');
+    });
+  });
+  function toggleActiveState(index, activeButtons, inactiveButtons) {
+    activeButtons[index].classList.add('active');
+    inactiveButtons[index].classList.remove('active');
+
+    // You can use 'selectedOptions' array for processing the selected options
+    console.log('Selected options:', selectedOptions);
+	console.log(activeButtons)
+  }
+});
